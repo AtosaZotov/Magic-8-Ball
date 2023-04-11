00cce110337d2878cec74e217b43b364d831f435
Magic 8 Ball <atozazotov@gmail.com>
2023-04-11 04:52:17 +0330

    Initial Commit

---
+++ Magic 8 Ball1UITests/Magic_8_Ball1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Magic_8_Ball1UITestsLaunchTests.swift
+//  Magic 8 Ball1UITests
+//
+//  Created by Yasamin  on 1/22/1402 AP.
+//
+
+import XCTest
+
+final class Magic_8_Ball1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
